4c5ab8959834dd07307b98212a0767d2ad7db05b
priyaclove <priya.cloveode@gmail.com>
2025-06-01 19:47:44 +0530

    correct

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -2,7 +2,7 @@
 
 import { motion } from "framer-motion";
 import Link from "next/link";
-import { Mail, Phone, Send, MapPin } from "lucide-react";
+import { Phone, Send, MapPin } from "lucide-react";
 import {
   FaFacebookF,
   FaInstagram,
--- src/components/contact-us/contact-us.tsx
+++ src/components/contact-us/contact-us.tsx
@@ -2,7 +2,6 @@
 
 import React, { useState } from "react";
 import { 
-  FiMail, 
   FiPhone, 
   FiSend, 
   FiCheckCircle, 
